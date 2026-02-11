65aec8ab489fe4391c4055c0d7a919013579f31e
FlourishingWyrm <141595341+FlourishingWyrm@users.noreply.github.com>
2026-02-11 13:15:58 +1300

    Add files via upload

--- v2.py
+++ v2.py
@@ -9,6 +9,8 @@
 
 #return (user_input - 32) * 5 / 9 (celcus)
 from tkinter import *
+import all_constants as c
+import time
 class Converter():
     """conversion tool"""
 
@@ -34,53 +36,47 @@ class Converter():
         self.temp_entry.grid(row=2, padx=10, pady=10)
 
         error = "Please enter a valid temperature"
-        self.temp_error = Label(self.temp_frame,text = error,fg="#9C0000")
-        self.temp_error.grid(row=3)
+        self.answer_error = Label(self.temp_frame, text = error, fg="#004C99",
+                                  font=("Arial", 14, "bold"))
+        self.answer_error.grid(row=3)
         self.button_frame = Frame(self.temp_frame)
         self.button_frame.grid(row=4)
-
+        buttons = [
+            ["To celcus", "#990099",lambda: self.check_temp(c.ABS_ZERO_CELSIUS),0,0],
+            ["to Farenheight","#009900",lambda: self.check_temp(c.ABS_ZER0_FAHRENHEIT),0,1],
+            ["Help/info","#CC6600","",1,0],
+            ["History/Export","#004C99","",1,1]
+        ]
+        self.button_ref_list = []
         for item in buttons:
             self.make_button = Button(self.button_frame,
                                       text=item[0], bg=item[1],
-                                      fg="#FFFFFF")
+                                      fg="#FFFFFF",font=("Arial", 12, "bold"),
+                                      width=12,command=item[2])
+            self.make_button.grid(row=item[3], column=item[4], padx=5, pady=5)
 
-        self.to_celsius_button = Button(self.button_frame,
-                                        text="to Celsius",
-                                        bg="#990099",
-                                        font=("Arial", 12, "bold"), width=12)
-        self.to_celsius_button.grid(row=0, column=0, padx=5, pady=5)
+            self.button_ref_list.append(self.make_button)
+        self.to_history_button = self.button_ref_list[3].config(state=DISABLED)
+    def check_temp(self, min_temp,valid=False):
+        """checks if the temperature is valid and converts it to the selected unit"""
+        try:
+            to_convert = float(self.temp_entry.get().strip(["°F","°C"][[-459,-273].index(min_temp)]))
+            if (to_convert >= min_temp):
+                error = ""
+                valid = True
+            else:
+                error = f"temperature must be greater than or equal to {min_temp}"
+        except ValueError:
+            error="Please enter a valid temperature"
+        if error !="":
+            self.answer_error.config(text=error,fg="#9C0000")
+            self.temp_entry.config(bg="#F4CCCC")
+            # self.temp_entry.focus_set()
+        if valid == True:
+            self.temp_entry.delete(0, END)
+            self.answer_error.config(text=str(round([(to_convert* 9/5) + 32,(to_convert - 32) * 5/9][[-273,-459].index(min_temp)]+0.1)),font=("Arial", 18, "bold"))
 
-        self.button_frame = Frame(self.temp_frame)
-        self.button_frame.grid(row=4)
-
-        self.to_celsius_button = Button(self.button_frame,
-                                        text="to Celsius",
-                                        bg="#990099",
-                                        font=("Arial", 12, "bold"), width=12)
-        self.to_celsius_button.grid(row=0, column=0, padx=5, pady=5)
-
-        self.to_fahrenheit_button = Button(self.button_frame,
-                                           text="to Fahrenheit",
-                                           bg="#990099",
-                                           fg="#ffffff",
-                                           font=("Arial", 12, "bold"), width=12)
-        self.to_fahrenheit_button.grid(row=0, column=1, padx=5, pady=5)
-
-        self.to_help_button = Button(self.button_frame,
-                                     text="Help/info",
-                                     bg="#CC6600",
-                                     fg="#ffffff",
-                                     font=("Arial", 12, "bold"), width=12)
-        self.to_help_button.grid(row=1, column=0, padx=5, pady=5)
-
-        self.to_history_button = Button(self.button_frame,
-                                        text="History/ Export",
-                                        bg="#004C99",
-                                        fg="#ffffff",
-                                        font=("Arial", 12, "bold"), width=12)
-        self.to_history_button.grid(row=1, column=1, padx=5, pady=5)
-
-
+# [(to_convert× 9/5) + 32,(to_convert − 32) × 5/9]
 #main code
 if __name__ == '__main__':
     root = Tk()
